6c30f8e4a83c87178cd9e7789387c1669d69d80c
Bardia Daneshvar <bardia.daneshvar@gmail.com>
2016-01-03 21:05:14 +0330

    Crash bugfix on null fileHandler

--- telegram.cpp
+++ telegram.cpp
@@ -1918,9 +1918,11 @@ qint64 Telegram::updatesGetDifference(qint32 pts, qint32 date, qint32 qts) {
 }
 
 qint64 Telegram::uploadGetFile(const InputFileLocation &location, qint32 fileSize, qint32 dcNum, const QByteArray &key, const QByteArray &iv) {
+    if(!prv->mFileHandler) return 0;
     return prv->mFileHandler->uploadGetFile(location, fileSize, dcNum, key, iv);
 }
 
 qint64 Telegram::uploadCancelFile(qint64 fileId) {
+    if(!prv->mFileHandler) return 0;
     return prv->mFileHandler->uploadCancelFile(fileId);
 }
